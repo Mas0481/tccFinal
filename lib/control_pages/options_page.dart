796937ebe7df3568c7a404bba868f250910953b3
Marcio Almeida <mas0481@hotmail.com>
2024-12-08 18:30:07 -0300

    Implementada a tela de detalhes do pedido e exclusão de pedidos.

--- lib/control_pages/options_page.dart
+++ lib/control_pages/options_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tcc/forms/form_novoPedido.dart';
+import 'package:tcc/forms/pedidos.dart';
 import 'package:tcc/util/custom_appbar.dart';
 import 'routes.dart';
 
@@ -30,38 +31,73 @@ class OptionsPage extends StatelessWidget {
           ],
         ),
       ),
-      floatingActionButton: Container(
-          margin: const EdgeInsets.only(
-              bottom: 40, right: 40), // Eleva o botão para cima
-          child: ElevatedButton(
-            onPressed: () {
-              // Exibe o formulário de novo pedido na frente da tela de opções
-              showDialog(
-                context: context,
-                builder: (BuildContext context) {
-                  return NovoPedido(
-                    onSave: () {
-                      // Lógica para salvar o pedido
-                      // Exemplo: Chamar algum método para salvar o pedido no banco ou realizar a ação desejada
-                    },
-                  );
-                },
-              );
-            },
-            style: ElevatedButton.styleFrom(
-              padding: const EdgeInsets.symmetric(vertical: 24),
-              backgroundColor:
-                  const Color.fromARGB(255, 238, 233, 237), // Cor do botão
-              foregroundColor: const Color.fromARGB(255, 119, 90, 113),
-              side: BorderSide(color: Color.fromARGB(255, 238, 233, 237)),
-              shape: CircleBorder(),
+      floatingActionButton: Stack(
+        children: [
+          // Botão de Novo Pedido
+          Positioned(
+            bottom: 40,
+            right: 40,
+            child: ElevatedButton(
+              onPressed: () {
+                // Exibe o formulário de novo pedido na frente da tela de opções
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return NovoPedido(
+                      onSave: () {
+                        // Lógica para salvar o pedido
+                        // Exemplo: Chamar algum método para salvar o pedido no banco ou realizar a ação desejada
+                      },
+                    );
+                  },
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                backgroundColor:
+                    const Color.fromARGB(255, 238, 233, 237), // Cor do botão
+                foregroundColor: const Color.fromARGB(255, 119, 90, 113),
+                side: BorderSide(color: Color.fromARGB(255, 238, 233, 237)),
+                shape: CircleBorder(),
+              ),
+              child: Text(
+                '+',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 119, 90, 113), fontSize: 30),
+              ),
             ),
-            child: Text(
-              '+',
-              style: TextStyle(
-                  color: Color.fromARGB(255, 119, 90, 113), fontSize: 30),
+          ),
+          // Botão de Informações
+          Positioned(
+            bottom: 130, // Distância agradável do botão "+"
+            right: 40,
+            child: ElevatedButton(
+              onPressed: () {
+                // Exibe o formulário de informações na frente da tela de opções
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return Pedidos(); // Chama o formulário de informações
+                  },
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(vertical: 24),
+                backgroundColor:
+                    const Color.fromARGB(255, 238, 233, 237), // Cor do botão
+                foregroundColor: const Color.fromARGB(255, 119, 90, 113),
+                side: BorderSide(color: Color.fromARGB(255, 238, 233, 237)),
+                shape: CircleBorder(),
+              ),
+              child: Text(
+                'i',
+                style: TextStyle(
+                    color: Color.fromARGB(255, 119, 90, 113), fontSize: 30),
+              ),
             ),
-          )),
+          ),
+        ],
+      ),
     );
   }
 
